bb88d7d40474aa889d001ca599376ff497369684
faze-useless <143573781+faze-useless@users.noreply.github.com>
2023-10-26 21:04:10 -0600

    Add files via upload

---
+++ EVA2_13_CICLO_FOR/src/eva2_13_ciclo_for/EVA2_13_CICLO_FOR.java
@@ -0,0 +1,23 @@
+package eva2_13_ciclo_for;
+import java.util.*;
+/**
+ *
+ * @author DELL
+ */
+public class EVA2_13_CICLO_FOR {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Scanner keyboard = new Scanner(System.in);
+        //int nocalif;
+        System.out.println("Ciclo for:");
+        for(int i = 0; i < 10; i++){
+            System.out.println(i);
+        }
+        System.out.println("-------------");
+        for(int i = 9; i >= 0; i--){
+            System.out.println(i);
+        }
+    }
+    
+}
