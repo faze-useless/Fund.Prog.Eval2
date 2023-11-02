15d48e84a94983b4f3da0e014ea18253797d9d11
faze-useless <143573781+faze-useless@users.noreply.github.com>
2023-11-01 22:23:27 -0600

    Add files via upload

---
+++ EVA2_18_DO_WHILE/src/a/A.java
@@ -0,0 +1,25 @@
+
+package a;
+import java.util.*;
+/**
+ *
+ * @author DELL
+ */
+public class A {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num;
+        Scanner keyboard = new Scanner(System.in);
+        
+        do{
+            System.out.println("Intenta adivinar el numero: ");
+            num = keyboard.nextInt();
+            
+        }while(num != 100);
+        
+        System.out.println("LE ATINASTE XDXDDDXD");
+        
+    }
+    
+}
